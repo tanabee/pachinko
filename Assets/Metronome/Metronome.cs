ca0b019d44d5c181792c6996113b83b70e655338
tanabee <tanabee1986@gmail.com>
2015-04-22 18:20:17 +0900

    開閉のバー追加

---
+++ Assets/Metronome/Metronome.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Metronome : MonoBehaviour {
+
+	public float speed;
+	public float startAngle;
+	public float endAngle;
+
+	// Update is called once per frame
+	void Update () {
+
+		// sin 値を計算
+		float value = Mathf.Abs(Mathf.Sin (Time.time));
+		float angle = Mathf.Lerp(startAngle, endAngle, value);
+
+		//transform.rotation = Quaternion.Euler(0f, angle, 0f);
+		transform.localRotation = Quaternion.Euler (0f, angle, 0f);
+
+	}
+}
